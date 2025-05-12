37682f7df66b3dbe96ac8720a61c63be9eefc4d0
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-11 23:19:08 -0300

    Add Double extension and CoinRow

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,73 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Dmitry Volkov on 11/05/2025.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: Coin
+    let showHoldingsColumn: Bool
+   
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            
+            if showHoldingsColumn {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: MockData().coin, showHoldingsColumn: true)
+}
+
+extension CoinRowView {
+    
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundStyle(Color.theme.accent)
+        }
+    }
+    
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing){
+            Text(coin.currentPrice.asCurrencyWith2Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0 >= 0) ?
+                    Color.theme.green :
+                    Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
