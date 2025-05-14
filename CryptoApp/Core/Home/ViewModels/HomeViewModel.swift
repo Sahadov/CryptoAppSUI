2f0ab161261e8f6feb861f3b312c6cbd7807b6e7
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-14 18:26:27 -0500

    Add coins lits

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(MockData().coin)
+            self.portfolioCoins.append(MockData().coin)
+        }
+    }
+}
