3aefb96e8a54ac5d5d44af295c896d005c111382
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-15 18:01:53 -0500

    Add combine coinDataService

---
+++ CryptoApp/Core/Components/CoinImage/CoinImageView.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageView.swift
+//  CryptoApp
+//
+//  Created by Dmitry Volkov on 15/05/2025.
+//
+
+import SwiftUI
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: Coin) {
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
+#Preview {
+    CoinImageView(coin: MockData().coin)
+}
---
+++ CryptoApp/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  CryptoApp
+//
+//  Created by Dmitry Volkov on 15/05/2025.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    private var imageSubscription: AnyCancellable?
+    private var coin: Coin
+    
+    init(coin: Coin) {
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {
+        guard let url = URL(string: coin.image)
+        else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
