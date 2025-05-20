4461b67b1ea2ec7515942258c7976819b1355f7b
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-20 14:35:06 -0500

    Add market data combine service

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,37 +9,36 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
-    @Published var statistics: [Statistic] = [
-        Statistic(title: "Mock1", value: "$12Bn", percantageChange: 10.4),
-        Statistic(title: "Mock2", value: "$17Tr"),
-        Statistic(title: "Mock3", value: "$10.5Bn"),
-        Statistic(title: "Mock4", value: "$12Bn", percantageChange: -10.4)
-    ]
+    @Published var statistics: [Statistic] = []
     
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
     
     init() {
         addSubscribers()
     }
     
-    func addSubscribers() {
-//        dataService.$allCoins
-//            .sink { [weak self] (returnedCoins) in
-//                self?.allCoins = returnedCoins
-//            }
-//            .store(in: &cancellables)
-        
+    func addSubscribers() {        
+        // update coins data
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        // market data
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cancellables)
     }
@@ -56,5 +55,25 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData(marketData: MarketData?) -> [Statistic] {
+        var stats: [Statistic] = []
+        
+        guard let data = marketData else { return stats }
+        
+        let marketCap = Statistic(title: "Market Cap", value: data.marketCap, percantageChange: data.marketCapChangePercentage24HUsd)
+        let volume = Statistic(title: "24h Volume", value: data.volume)
+        let btcDominance = Statistic(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = Statistic(title: "Portfolio Value", value: "$0.00", percantageChange: 0)
+        
+        stats.append(contentsOf: [
+            marketCap,
+            volume,
+            btcDominance,
+            portfolio
+        ])
+        
+        return stats
     }
 }
---
+++ CryptoApp/Services/MarketDataService.swift
@@ -0,0 +1,30 @@
+//
+//  MarketDataService.swift
+//  CryptoApp
+//
+//  Created by Dmitry Volkov on 18/05/2025.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    @Published var marketData: MarketData? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global")
+        else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedData in
+                self?.marketData = returnedData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
